c4f9506cf7b363e89311e14213c52f126c432e66
Cr-Israel <carlosisrael08@hotmail.com>
2023-07-08 11:39:38 -0300

    feat: create use-case and test conclude

---
+++ src/use-cases/create-appointment.ts
@@ -0,0 +1,25 @@
+import { Appointment } from "../entities/appointment";
+
+interface CreateAppointmentRequest {
+  costumer: string;
+  startsAt: Date;
+  endsAt: Date;
+}
+
+type CreateAppointmentResponse = Appointment;
+
+export class CreateAppointment {
+  async execute({
+    costumer,
+    startsAt,
+    endsAt,
+  }: CreateAppointmentRequest): Promise<CreateAppointmentResponse> {
+    const appointment = new Appointment({
+      costumer,
+      startsAt,
+      endsAt,
+    });
+
+    return appointment;
+  }
+}
